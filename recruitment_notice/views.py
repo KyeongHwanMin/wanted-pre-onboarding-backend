532c1ddba8f271968402e907c2fa2c8370ff2f9d
KyeongHwan Min <h45652@naver.com>
2023-10-13 11:09:38 +0900

    FEAT : recruitment_notice 채용공고 검색기능 구현

--- recruitment_notice/views.py
+++ recruitment_notice/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -12,7 +13,18 @@ from recruitment_notice.serializers import (
 
 class RecruitmentNoticeView(APIView):
     def get(self, request):
-        qs = RecruitmentNotice.objects.all()
+        search = request.query_params.get("search", "")
+        if search:
+            qs = RecruitmentNotice.objects.filter(
+                Q(position__icontains=search)
+                | Q(skill__icontains=search)
+                | Q(company__name__icontains=search)
+                | Q(company__nation__icontains=search)
+                | Q(company__area__icontains=search)
+            )
+
+        else:
+            qs = RecruitmentNotice.objects.all()
         serializer = RecruitmentNoticeListSerializer(qs, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
